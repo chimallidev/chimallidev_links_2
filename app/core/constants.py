49138a1b2eb14ee3b9059ec3c0bad371a70973ad
chimallidev <chimalli.dev@gmail.com>
2026-05-26 16:09:30 -0600

    plantilla base del proyecto

---
+++ app/core/constants.py
@@ -0,0 +1,5 @@
+#META
+META = {
+    "titulo": "Desarrollador",
+    "descripcion": "Vacío"
+}
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import HTMLResponse
 from app.core.templates import templates
+from app.core.constants import META
 
 router = APIRouter()
 
 @router.get("/", response_class= HTMLResponse, status_code= status.HTTP_200_OK)
 async def home(request: Request):
+    context = {
+        "meta": META
+    }
+
     try:
-        return templates.TemplateResponse(request= request, name= "index.html", context= {})
+        return templates.TemplateResponse(request= request, name= "index.html", context= context)
     except:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                             detail= "La página no ha sido encontrada.")
