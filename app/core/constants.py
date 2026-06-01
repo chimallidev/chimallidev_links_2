49c9f95f8f1b699a32ede34a5f881288b9f7d482
chimallidev <chimalli.dev@gmail.com>
2026-06-01 16:20:56 -0600

    Main buttons

--- app/core/constants.py
+++ app/core/constants.py
@@ -31,3 +31,76 @@ HEADER_LINKS = [
         "name": "enlace a x"
     }
 ]
+
+#Botones principales
+MAIN_BUTTONS = [
+    {
+        "type": "link",
+
+        "url": "#",
+
+        "open_new_tab": False,
+
+        "state": "turquoise",
+
+        "border_state": "active",
+
+        "svg_id": "icon-code-slash",
+
+        "title": "Cursos",
+
+        "description": "Recursos gratis de programación"
+    },
+    {
+        "type": "link",
+
+        "url": "https://www.youtube.com/@Chimallidev",
+
+        "open_new_tab": True,
+
+        "svg_id": "icon-youtube",
+
+        "title": "Youtube",
+
+        "description": "Videos sobre programación"
+    },
+    {
+       "type": "link",
+
+        "url": "https://tiktok.com/@chimallidev",
+
+        "open_new_tab": True,
+
+        "svg_id": "icon-tiktok",
+
+        "title": "Tik Tok",
+
+        "description": "Videos cortos sobre pogramación"
+    },
+    {
+        "type": "link",
+
+        "url": "https://instagram.com/chimallidev",
+
+        "open_new_tab": True,
+
+        "svg_id": "icon-instagram",
+
+        "title": "Instagram",
+
+        "description": "Mi día a día como programador"
+    },
+    {
+        "type": "link",
+
+        "url": "https://ko-fi.com/chimallidev",
+
+        "open_new_tab": True,
+
+        "svg_id": "icon-cup-hot-fill",
+
+        "title": "Invítame un café",
+
+        "description": "¿Quieres apoyarme?"
+    }
+]
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import HTMLResponse
 from app.core.templates import templates
-from app.core.constants import META, HEADER_LINKS
+from app.core.constants import META, HEADER_LINKS, MAIN_BUTTONS
 
 router = APIRouter()
 
@@ -9,7 +9,8 @@ router = APIRouter()
 async def home(request: Request):
     context = {
         "meta": META,
-        "header_links": HEADER_LINKS
+        "header_links": HEADER_LINKS,
+        "main_buttons": MAIN_BUTTONS
     }
 
     try:
