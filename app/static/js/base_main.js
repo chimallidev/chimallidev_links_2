334b3129dc38d921ca9fea00b20fd34627cdd39f
chimallidev <chimalli.dev@gmail.com>
2026-06-05 19:43:59 -0600

    Componente nav_bar

---
+++ app/static/js/base_main.js
@@ -0,0 +1,10 @@
+import { initNavbar } from './nav_bar.js';
+
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        console.log("Mensaje de la base")
+        initNavbar();
+
+    }
+);
