db696e452885108968d3d2420c7930be935cbae0
chimallidev <chimalli.dev@gmail.com>
2026-06-08 14:54:40 -0600

    Footer

--- app/routes/home.py
+++ app/routes/home.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import HTMLResponse
 from app.core.templates import templates
 from app.core.constants import META, HEADER_LINKS, MAIN_BUTTONS, DESTACADOS_CARDS, CONTACTO_BUTTONS
+from app.utils.datetime import get_current_year
 
 router = APIRouter()
 
@@ -12,7 +13,8 @@ async def home(request: Request):
         "header_links": HEADER_LINKS,
         "main_buttons": MAIN_BUTTONS,
         "contacto_buttons": CONTACTO_BUTTONS,
-        "destacados_cards": DESTACADOS_CARDS
+        "destacados_cards": DESTACADOS_CARDS,
+        "footer_year": get_current_year()
     }
 
     try:
