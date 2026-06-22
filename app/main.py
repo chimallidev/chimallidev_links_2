5f8f16ad9f89e3cc4af43e2aa7059edfebde9d8c
chimallidev <chimalli.dev@gmail.com>
2026-06-22 16:26:26 -0600

    refactor: sistema adapatado para su uso con git subtree

--- app/main.py
+++ app/main.py
@@ -1,17 +1,3 @@
-from pathlib import Path
-from fastapi import FastAPI
-from app.core.templates import templates
-from fastapi.staticfiles import StaticFiles
-from app.routes.home import router as home_router
-from app.routes.cursos import router as cursos_router
+from app.application import create_app
 
-BASE_DIR = Path(__file__).resolve().parent
-
-app = FastAPI()
-
-#Archivos estáticos (infraestructura)
-app.mount("/static", StaticFiles(directory= BASE_DIR / "static"), name= "static")
-
-#Rutas
-app.include_router(home_router)
-app.include_router(cursos_router)
+app = create_app()
